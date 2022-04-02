ebaf61de26707fcae42b96a7c9d1145d961f45a9
ria.acharya07 <ria.acharya07@gmail.com>
2022-04-02 23:49:21 +0545

    categorycard updated

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import Product from "../components/Product/Product";
-import CategorySideBar from "../components/Category/CategorySideBar"
+import ProductContainer from "../components/Product/ProductContainer";
+
 
 const Products = () => {
   return (
-    <div className="products">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 col-md-12">
-            <CategorySideBar />
-          </div>
-          <div className="col-lg-8 col-md-12">
-            <Product />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+    <ProductContainer />
+    </>
+            
+     
   );
 };
 
